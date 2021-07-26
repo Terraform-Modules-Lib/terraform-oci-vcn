c11f600d49c19e7f041877847c5e95f50fe5569a
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 09:45:20 +0200

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -10,9 +10,9 @@ module "private_subnets" {
 module "public_subnets" {
   for_each = { for subnet in local.pub_subnets: subnet.cidr => subnet }
   source  = "Terraform-Modules-Lib/subnet-public/oci"
-  
+
   name = each.value.name
-  cidr = each.value.name
+  cidr = each.value.cidr
 
   vcn_id = local.vcn.id
   internet_gw_id = local.gw.internet.id
