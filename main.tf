b98318fa672e653056a828d5082b9901bfe20402
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 08:52:21 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,20 +30,4 @@ resource "oci_core_vcn" "vcn" {
   dns_label = local.name
 }
 
-module "private_subnets" {
-  for_each = { for subnet in local.prv_subnets: subnet.cidr => subnet }
-  source  = "Terraform-Modules-Lib/subnet-private/oci"
-  
-  name = each.value.name
-  cidr = each.value.cidr
-  vcn_id = local.vcn.id
-}
 
-module "public_subnets" {
-  for_each = { for subnet in local.pub_subnets: subnet.cidr => subnet }
-  source  = "Terraform-Modules-Lib/subnet-public/oci"
-  
-  name = each.value.name
-  cidr = each.value.name
-  vcn_id = local.vcn.id
-}
