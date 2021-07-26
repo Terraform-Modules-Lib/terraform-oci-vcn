a30786a8ada01133c082eddb92ec790e16feddeb
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-26 08:52:38 +0200

    Create subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,17 @@
+module "private_subnets" {
+  for_each = { for subnet in local.prv_subnets: subnet.cidr => subnet }
+  source  = "Terraform-Modules-Lib/subnet-private/oci"
+  
+  name = each.value.name
+  cidr = each.value.cidr
+  vcn_id = local.vcn.id
+}
+
+module "public_subnets" {
+  for_each = { for subnet in local.pub_subnets: subnet.cidr => subnet }
+  source  = "Terraform-Modules-Lib/subnet-public/oci"
+  
+  name = each.value.name
+  cidr = each.value.name
+  vcn_id = local.vcn.id
+}
