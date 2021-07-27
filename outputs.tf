6ff6fa2110f40cf6c89ce112809a032422773411
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2021-07-27 14:23:44 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@ output "vcn" {
   value = local.vcn
   description = "Provides the oci_core_vcn resource in Oracle Cloud Infrastructure Core service."
 }
+
+output "subnets" {
+  value = merge({ for subnet in module.public_subnets:
+    subnet.subnet.id => subnet.subnet
+  }, { for subnet in module.private_subnets:
+    subnet.subnet.id => subnet.subnet
+  })
+}
